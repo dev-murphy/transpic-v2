582d0a0a1a6ba238177c3ad2d4e195135f8b0b78
dev-murphy <facey.murphy1@gmail.com>
2026-02-15 18:07:11 -0500

    feat: add tailwindcss to app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Transpic v2</h1>
-    <p>This is under construction</p>
+  <div
+    class="h-screen bg-neutral-900 flex flex-col items-center justify-center text-center"
+  >
+    <h1 class="text-white text-5xl font-bold">Transpic v2</h1>
+    <p class="w-[600px] mt-2 text-xl text-neutral-500">
+      Any convenient way of converting and compressing images. This app is
+      🚧<span class="font-bold">UNDER CONSTRUCTION</span>🚧.
+    </p>
   </div>
 </template>
