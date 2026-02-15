683f374b101a2d8e8c0f0dab2a66210b97177c16
dev-murphy <facey.murphy1@gmail.com>
2026-02-15 18:18:25 -0500

    feat: add basic dependencies for app

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
+import path from "path";
+
 import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 import Tailwindcss from "@tailwindcss/vite";
+import VueDevtools from "vite-plugin-vue-devtools";
 import Vue from "@vitejs/plugin-vue";
+import WebfontDownload from "vite-plugin-webfont-dl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [Vue(), Tailwindcss()],
+  plugins: [
+    AutoImport({
+      include: [/\.vue$/, /\.vue\?vue/],
+      imports: ["vue", "@vueuse/core"],
+      dts: "src/auto-imports.d.ts",
+    }),
+    Components({
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: "src/components.d.ts",
+    }),
+    Vue(),
+    VueDevtools(),
+    Tailwindcss(),
+    WebfontDownload(),
+  ],
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
 });
